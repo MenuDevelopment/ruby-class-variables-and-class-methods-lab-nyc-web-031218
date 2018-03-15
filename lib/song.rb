5a4dd9e3bdae88e217a7cc56064f8ea3599a6958
Manuel Grullon <astermanuelg@gmail.com>
2018-03-15 10:52:52 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@genres = []
+  @@count = 0
+  @@artists = []
+
+  def initialize (name, artist, genre)
+    @@artists  << artist
+    @@genres << genre
+    @@count += 1
+    self.artist = artist
+    self.genre = genre
+    self.name = name
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      genre_hash[genre] ||= 0
+      genre_hash[genre] += 1
+    end
+    genre_hash
+
+  end
+
+  def self.artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      artist_hash[artist] ||= 0
+      artist_hash[artist] += 1
+    end
+    artist_hash
+
+  end
+
+
+
+
+end
